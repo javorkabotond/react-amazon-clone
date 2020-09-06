de8f1cf005540be6e6e778338e267feeaf0ccbd4
javorkabotond <botondjvorka@gmail.com>
2020-09-06 17:00:51 +0200

    Tried react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon clone! </h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/checkout'>
+            <h1>Check out</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>Login</h1>
+          </Route>
+          <Route path='/'>
+            <h1>Home page!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
