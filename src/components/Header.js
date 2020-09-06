01b594b511b8935882beca5e5651adf5102b358e
javorkabotond <botondjvorka@gmail.com>
2020-09-06 18:47:16 +0200

    Build the navigation bar links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import logo from '../images/amazon_PNG11.png';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 
 function Header() {
   return (
@@ -16,6 +17,33 @@ function Header() {
       <div className="header_search">
         <input type="text" className="header_searchInput"/>
         <SearchIcon className="header_searchIcon"/>
+      </div>
+
+      <div className="header_nav">
+        <Link to="/login" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Hello: </span>
+            <span className="header_optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        <Link to="/" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <Link to="/" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+          </div>
+        </Link>
+        <Link to="/checkout" className="header_link">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon/>
+            <span className="header_optionLineTwo header_basketCount">0</span>
+          </div>
+        </Link>
       </div>
      
     </nav>
