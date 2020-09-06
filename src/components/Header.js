8ebcfceca91b3d583131ad3b77ad557ef8198af3
javorkabotond <botondjvorka@gmail.com>
2020-09-06 17:41:44 +0200

    Build the navigation bar search input

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../images/amazon_PNG11.png';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
   return (
@@ -12,6 +13,11 @@ function Header() {
           src={logo}
           alt="" />
       </Link>
+      <div className="header_search">
+        <input type="text" className="header_searchInput"/>
+        <SearchIcon className="header_searchIcon"/>
+      </div>
+     
     </nav>
   )
 }
